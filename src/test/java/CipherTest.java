a393a8ba26aefa697b846da296c4717dfcfad9a2
Nicholas M <nicholasmuturi1@gmail.com>
2019-10-05 17:35:51 +0300

    [Test]: Passed - Test to check whether cipher object instantiates correctly

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -1,7 +1,13 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class CipherTest {
 
-
+    @Test
+    public void runCipher_instanceOfCipher(){
+        Cipher cipher = new Cipher();
+        assertTrue(cipher instanceof Cipher);
+    }
 
 }
