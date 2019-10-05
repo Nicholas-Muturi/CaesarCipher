9419862f8216606568c4485aad9622b7da823e41
Nicholas M <nicholasmuturi1@gmail.com>
2019-10-05 18:36:53 +0300

    [Test]: Passed - Test to check whether cipher constructor and getter method works as expected

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,2 +1,15 @@
 public class Cipher {
+    private String inputString;
+
+    public Cipher(String inputString) {
+        this.inputString = inputString;
+    }
+
+    public String getInputString() {
+        return inputString;
+    }
+
+    public void setInputString(String inputString) {
+        this.inputString = inputString;
+    }
 }
--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -1,13 +1,20 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class CipherTest {
 
     @Test
-    public void runCipher_instanceOfCipher(){
-        Cipher cipher = new Cipher();
+    public void createCipher_instanceOfCipher(){
+        Cipher cipher = new Cipher("test string");
         assertTrue(cipher instanceof Cipher);
+    }
+
+    @Test
+    public void runGetInput_returnString_String(){
+        Cipher cipher = new Cipher("test string");
+        assertEquals("test string", cipher.getInputString());
     }
 
 }
